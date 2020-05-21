70dffe02cc97c4bb946cf1cc45287d6d41794338
kaishwarya <kumari_aishwarya@intuit.com>
2020-05-21 14:30:55 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+     name='timeConverter',  
+     version='2.0',
+     scripts=['TimeConverter'] ,
+     author="Kumari Aishwarya",
+     author_email="k.aishwarya15@gmail.com",
+     description="A tool to convert time across time zones",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
