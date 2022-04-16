e4121f77b943f207194e10b1f4d42916989a0886
kaishwarya <kumari_aishwarya@intuit.com>
2022-04-17 01:10:20 +0530

    fix

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
 setuptools.setup(
      name='timeConverter',  
-     version='3.1',
+     version='3.2',
      scripts=['TimeConverter'] ,
      author="Kumari Aishwarya",
      author_email="k.aishwarya15@gmail.com",
-     description="A tool to convert time across time zones",
+     description="A GUI tool to convert time across all time zones. To use this tool, "
+                 "just type 'timeConverter' on your terminal.",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
